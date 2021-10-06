fa42e02629a900523f8b74695152bb3c335d325f
Bastien Paul <bastien.paul@coppernic.fr>
2021-10-06 11:53:12 +0200

    List<Filter>.mergeFilters() is not adding filters to MergeFilter object, fix #10

--- treessence/src/main/java/fr/bipi/tressence/common/filters/MergeFilter.kt
+++ treessence/src/main/java/fr/bipi/tressence/common/filters/MergeFilter.kt
@@ -1,7 +1,17 @@
 package fr.bipi.tressence.common.filters
 
-class MergeFilter : Filter {
-    val filters = mutableListOf<Filter>()
+/**
+ * Merge several filters into one.
+ *
+ * - If one of filters wants to skip log, then this merge filter is skipping log.
+ * - If one of filter tells that it is not loggable, then this merge filter is not loggable as well.
+ */
+class MergeFilter(
+    filters: List<Filter> = emptyList()
+) : Filter {
+    val filters = mutableListOf<Filter>().apply {
+        addAll(filters)
+    }
 
     override fun skipLog(priority: Int, tag: String?, message: String, t: Throwable?): Boolean {
         filters.forEach {
@@ -26,6 +36,6 @@ fun List<Filter>.mergeFilters() = when {
     this.isEmpty() -> NoFilter.INSTANCE
     this.size == 1 -> this[0]
     else -> MergeFilter().apply {
-        this.filters.addAll(filters)
+        this.filters.addAll(this@mergeFilters)
     }
 }
--- treessence/src/test/java/fr/bipi/tressence/common/filters/MergeFilterTest.kt
+++ treessence/src/test/java/fr/bipi/tressence/common/filters/MergeFilterTest.kt
@@ -1,5 +1,8 @@
 package fr.bipi.tressence.common.filters
 
+import org.amshove.kluent.`should be instance of`
+import org.amshove.kluent.`should be`
+import org.amshove.kluent.`should contain same`
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -39,5 +42,24 @@ class MergeFilterTest {
         }
         assertFalse(filter.skipLog(0, "", "", null))
         assertTrue(filter.isLoggable(0, ""))
+    }
+
+    @Test
+    fun `empty list gives no filters`() {
+        emptyList<Filter>().mergeFilters().`should be instance of`(NoFilter::class)
+    }
+
+    @Test
+    fun `single list return its own filter`() {
+        val filter = PriorityFilter(1)
+        listOf(filter).mergeFilters().`should be`(filter)
+    }
+
+    @Test
+    fun `multi filter list return a merge filter containing all filters`() {
+        val prio = PriorityFilter(1)
+        val tag = TagFilter("tag")
+        val merge = listOf(prio, tag).mergeFilters() as MergeFilter
+        merge.filters.`should contain same`(listOf(prio, tag))
     }
 }
